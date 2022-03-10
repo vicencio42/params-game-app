42bcb45e3c8abe2549f31e03aef44b76076bc5fb
Vicencio42 <vicencio42@gmail.com>
2022-03-09 20:24:47 -0600

    Added routes adn controller to name/number games

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,7 +1,30 @@
 class ParamsController < ApplicationController
 
   def query_params
-    input_name = params("name")
-    render json: {message: "Your name is #{input_name}", message2: "Your number is #{params['x']}"}
-end
+    input_name = params["name"].upcase
+  end
+
+  def new_game
+    input_name = params["name"].upcase
+    if input_name[0] ==("A")
+      output_message = "Hey! Your name starts with A!"
+    else
+      output_message = "Cool beans!"
+    end
+    render json: {message: "Your name is #{input_name}. #{output_message}"}
+  end
+
+  def number_game
+    user_number_choice = params["user_number"].to_i
+    winning_number = 36
+    if user_number_choice < winning_number
+      output_message = "You guessed too low, guess higher!"
+    elsif user_number_choice > winning_number
+      output_message = "You guesed too high, guess lower!"
+    else
+      output_message = "You guessed it! You win!"
+    end
+    render json: {message_to_user: output_message}
+  end
+
 end
